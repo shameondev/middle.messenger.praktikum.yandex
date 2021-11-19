f216217007a14e270cf8d572547ce0b48fc322e2
Daniil Fadeev <shameondev@gmail.com>
2021-11-19 21:56:03 +0300

    Добавит PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('stylelint'),
+        require('postcss-nested'),
+        require('postcss-modules'),
+        require('postcss-simple-vars'),
+        require('postcss-mixins')
+    ]
+}
