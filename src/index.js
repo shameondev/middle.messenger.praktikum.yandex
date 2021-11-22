d8f9fe602687cf2fa3343c5820f441e3f065de1a
Daniil Fadeev <shameondev@gmail.com>
2021-11-22 22:10:25 +0300

    Добавит страницу регистрации

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import { NotFoundPage } from './pages/NotFoundPage';
 import './index.css';
+
+import { NotFoundPage } from './pages/NotFoundPage';
 import { ServerErrorPage } from './pages/ServerErrorPage';
 import { HomePage } from './pages/HomePage';
+import { SignUpPage } from './pages/SignUpPage';
+
+// TODO: Подумать что делать с ужасной лапшой кода в CSS
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.location.href.split('/')[3] === '') {
     document.body.innerHTML = HomePage;
   } else if (document.location.href.split('/')[3] === 'signup') {
-    document.body.innerHTML = 'signup';
+    document.body.innerHTML = SignUpPage;
   } else if (document.location.href.split('/')[3] === 'login') {
     document.body.innerHTML = 'login';
   } else if (document.location.href.split('/')[3] === 'user') {
